6b4c5c0c16bd1d40abfeff4480636fb39b4e7eab
Cristopher Paes de Barros <69487934+christopherjpb@users.noreply.github.com>
2024-12-07 04:45:24 +0000

    corrigindo commit errado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Minha filha papai te ama muito, estou me esforçando para melhorar nossas vidas.</h1>
+    return <h1>Minha filha papai te ama muito, estou me esforçando para melhorar nossas vidas</h1>
 }
 
 export default Home;
